e71d137edbe64c319aeef1df26048ff011ebde04
Fernando M Mattos <etcloide@gmail.com>
2023-05-03 18:02:54 -0300

    n sei

--- Tela_Principal/Tela_Reservas.py
+++ Tela_Principal/Tela_Reservas.py
@@ -1,7 +1,59 @@
 from tkinter import *
+janela = Tk()
+
+def exibir():
+        if (cadastro.radio_value.get() == 1):
+            BtnBox.configure(bg="green")
+        elif (cadastro.radio_value.get() == 2):
+            BtnBox.configure(bg="red")
+
+def BtnBox(quarto, x, y):
+    btnBox = Button(janela, text=quarto, font=(quarto, 25),bg="green", fg="white", width=5, height=2, command=cadastro)
+    btnBox.pack()
+    btnBox.place(x=x, y=y)
+
+def Tela():
+    janela.geometry("1024x768")
+    janela.minsize(1024, 768)
+    janela.maxsize(1920, 1080)
+    janela.title("Tela de reservas")
+
+    rotuloDoTitulo = Label(janela, bg="dark blue", width=180, height=6)
+    rotuloDoTitulo.pack()
+
+    titulo1 = Label(janela, text="Wyden Hotel", font=("Wyden Hotel", 20), bg="dark blue", fg="white",)
+    titulo1.pack()
+    titulo1.place(x=640, y=25)
+
+    titulo2 = Label(janela, text="Quartos:", font=("Quartos", 18))
+    titulo2.pack()
+
+    BtnBox("5A", 475, 150)
+    BtnBox("5B", 600, 150)
+    BtnBox("5C", 725, 150)
+    BtnBox("5D", 850, 150)
+    BtnBox("4A", 475, 275)
+    BtnBox("4B", 600, 275)
+    BtnBox("4C", 725, 275)
+    BtnBox("4D", 850, 275)
+    BtnBox("3A", 475, 400)
+    BtnBox("3B", 600, 400)
+    BtnBox("3C", 725, 400)
+    BtnBox("3D", 850, 400)
+    BtnBox("2A", 475, 525)
+    BtnBox("2B", 600, 525)
+    BtnBox("2C", 725, 525)
+    BtnBox("2D", 850, 525)
+    BtnBox("1A", 475, 650)
+    BtnBox("1B", 600, 650)
+    BtnBox("1C", 725, 650)
+    BtnBox("1D", 850, 650)
+
+    janela.mainloop()
+
+#JANELA DE CADASTRO/CHECK-IN
 
 def cadastro():
-    janela = Tk()
     screenRoom = Toplevel(janela)
     screenRoom.geometry("525x450")
     screenRoom.minsize(525, 450)
@@ -12,7 +64,8 @@ def cadastro():
     radio_value = IntVar()
     # Aqui estão as funçoes que aparecerem na tela de cadastro( labels e inputs)
     labelRoom = Label(screenRoom, bg="midnight blue",fg="white", width=100, height=3).pack()
-    nameLabelRoom = Label(screenRoom, text="Quarto 1A:", font=("Quarto 1A:", 18), bg="midnight blue", fg="white")
+    
+    nameLabelRoom = Label(screenRoom, text="Quarto 1A:", bg="midnight blue", fg="white")
     nameLabelRoom.pack()
     nameLabelRoom.configure(font=Font_tuple)
     nameLabelRoom.place(x=10, y=12)
@@ -59,6 +112,6 @@ def cadastro():
     radiobtn1.place(x=380, y=130)
     radiobtn2.pack()
     radiobtn2.place(x=370, y=160)
-    btnChangeColor1A = Button(screenRoom, text="SALVAR", bg="black", fg="white", width=8, height=2)
+    btnChangeColor1A = Button(screenRoom, text="SALVAR", bg="black", fg="white", width=8, height=2, command=exibir)
     btnChangeColor1A.pack()
     btnChangeColor1A.place(x=380, y=190)
