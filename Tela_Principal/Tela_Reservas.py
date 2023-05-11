b1c255faf82ec6bada63351130ee4d13a8197241
Fernando M Mattos <etcloide@gmail.com>
2023-05-11 04:18:06 -0300

    calendario update

--- Tela_Principal/Tela_Reservas.py
+++ Tela_Principal/Tela_Reservas.py
@@ -1,12 +1,11 @@
 from tkinter import *
 import tkinter as tk
 from database import *
-from tkcalendar import Calendar
+from tkcalendar import Calendar, DateEntry
 from datetime import datetime 
 
 database = RoomDB()
-janela = Tk()
-calendario= Calendar(mindate = datetime(2023,1,1), maxdate = datetime(2023,12,31), showweeknumbers = False, showothermonthdays = False)
+root = tk.Tk()
          
 class tela:
     radio_value= IntVar()
@@ -19,45 +18,52 @@ class tela:
 
     @staticmethod
     def BtnBox(quarto, x, y):
-        btnBox = Button(janela, text=quarto, font=(quarto, 25),bg="green", fg="white", width=5, height=2, command=lambda: tela.cadastro(quarto))
+        btnBox = Button(root, text=quarto, font=(quarto, 25),bg="green", fg="white", width=5, height=2, command=lambda: tela.cadastro(quarto))
         btnBox.pack()
         btnBox.place(x=x, y=y)
 
     @staticmethod
     def DateBox():
-        def updateLabel():
-            data = calendario.get_date()
-            label.config(text="Data Selecionada: " + data)
-            
-        #checkOutLabel = Label(janela, text="Check-out:",font=("Check-out", 15))
-        #checkOutLabel.pack()
-        #checkOutLabel.place(x=30, y=180)
+        def get_dates():
+            checkin = cal.selection
+            checkout = cal.get_date() + " 12:00:00"
+            print("Data Check-in:", checkin)
+            print("Data Check-out:", checkout)
+        cal = Calendar(root, mindate = datetime(2023,1,1), maxdate = datetime(2023,12,31), showweeknumbers = False, showothermonthdays = False)
 
-        #checkOutEntry = Entry(janela, width=15, font=("calibri", 15))
-        #checkOutEntry.pack()
-        #checkOutEntry.place(x=30, y=220)
-        calendario.pack()
-        calendario.place(x=30, y=100)
+        Label1 = tk.Label(root, text="Data Check-in:")
+        Label1.pack()
+        Label1.place(x=30,y=300)
 
-        calendario.bind("<<CalendarSelected>>", updateLabel)
+        checkin_date = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2)
+        checkin_date.pack(padx=10, pady=10)
+        checkin_date.place(x=30,y=325)
 
-        label = tk.Label(text="Data Selecionada: ")
-        label.pack()
+        Label2 = tk.Label(root, text="Data Check-out:")
+        Label2.pack()
+        Label2.place(x=30,y=350)
+
+        checkout_date = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2)
+        checkout_date.pack(padx=10, pady=10)
+        checkout_date.place(x=30,y=375)
+
+        button = tk.Button(root, text="Get dates", command=get_dates)
+        button.pack(pady=20)
 
     def Tela(): 
-        janela.geometry("1024x768")
-        janela.minsize(1024, 768)
-        janela.maxsize(1920, 1080)
-        janela.title("Tela de reservas")
+        root.geometry("1024x768")
+        root.minsize(1024, 768)
+        root.maxsize(1920, 1080)
+        root.title("Tela de reservas")
 
-        rotuloDoTitulo = Label(janela, bg="dark blue", width=180, height=6)
+        rotuloDoTitulo = Label(root, bg="dark blue", width=180, height=6)
         rotuloDoTitulo.pack()
 
-        titulo1 = Label(janela, text="Wyden Hotel", font=("Wyden Hotel", 20), bg="dark blue", fg="white",)
+        titulo1 = Label(root, text="Wyden Hotel", font=("Wyden Hotel", 20), bg="dark blue", fg="white",)
         titulo1.pack()
         titulo1.place(x=640, y=25)
 
-        titulo2 = Label(janela, text="Quartos:", font=("Quartos", 18))
+        titulo2 = Label(root, text="Quartos:", font=("Quartos", 18))
         titulo2.pack()
 
         tela.DateBox()
@@ -82,12 +88,12 @@ class tela:
         tela.BtnBox("1C", 725, 650)
         tela.BtnBox("1D", 850, 650)
 
-        janela.mainloop()
+        root.mainloop()
 
-    #JANELA DE CADASTRO/CHECK-IN
+    #root DE CADASTRO/CHECK-IN
 
     def cadastro(quarto):
-        screenRoom = Toplevel(janela)
+        screenRoom = Toplevel(root)
         screenRoom.geometry("525x450")
         screenRoom.minsize(525, 450)
         screenRoom.maxsize(525, 450)
