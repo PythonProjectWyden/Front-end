bd5db7003d2f10d2e6cbc0328fc7ed1607719424
CycleWs <joaohlspj@gmail.com>
2023-05-16 09:38:25 -0300

    GG CPF agr vai

--- Tela_Principal/Main.py
+++ Tela_Principal/Main.py
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 from tkinter import *
 from Tela_Reservas import tela
+=======
+from TelaTeste import *
+>>>>>>> Stashed changes
 
 tela.Tela()
--- Tela_Principal/TelaTeste.py
+++ Tela_Principal/TelaTeste.py
@@ -1,12 +1,58 @@
-from tkinter import *
 import tkinter as tk
+from tkinter import *
 from database import *
-import telaResumo
+from tkcalendar import DateEntry
+from datetime import datetime 
 database = RoomDB()
 
-telaDois = None
-Quarto = None
+class telasDeData:
+    def proxTela():
+        telasDeQuartos.segundaTela()
+    
+    def primeiraTela():
+        global checkinDentro, checkoutDentro
+        telaUm = tk.Tk()
+        telaUm.geometry("300x300")
+        telaUm.minsize(300, 300)
+        telaUm.maxsize(300, 300)
+        telaUm.title("Check-In e Check-Out")
+        rotuloDoTituloTelaUm = Label(telaUm, bg="dark blue", width=180, height=6)
+        rotuloDoTituloTelaUm.pack()
 
+        tituloTelaUm1 = Label(telaUm, text="Wyden Hotel", font=("Wyden Hotel", 20), bg="dark blue", fg="white",)
+        tituloTelaUm1.pack()
+        tituloTelaUm1.place(x=70, y=30)
+
+        tituloTelaUm2 = Label(telaUm, text="Datas:", font=("Datas", 18))
+        tituloTelaUm2.pack()
+
+        LabelTelaUm1 = tk.Label(telaUm, text="Data Check-in:")
+        LabelTelaUm1.pack()
+        LabelTelaUm1.place(x=100,y=130)
+
+        checkin_date = DateEntry(telaUm, width=12, background='darkblue', foreground='white', borderwidth=2, mindate = datetime(2023,1,1),
+                                  maxdate = datetime(2023,12,31), showweeknumbers = False, showothermonthdays = False)
+        checkin_date.pack(padx=10, pady=10)
+        checkin_date.place(x=100,y=150)
+
+        LabelTelaUm2 = tk.Label(telaUm, text="Data Check-out:")
+        LabelTelaUm2.pack()
+        LabelTelaUm2.place(x=100,y=180)
+
+        checkout_date = DateEntry(telaUm, width=12, background='darkblue', foreground='white', borderwidth=2, mindate = datetime(2023,1,1),
+                                   maxdate = datetime(2023,12,31), showweeknumbers = False, showothermonthdays = False)
+        checkout_date.pack(padx=10, pady=10)
+        checkout_date.place(x=100,y=200)
+
+        continuar = Button(telaUm,text="Salvar Datas", bg="dark blue",fg="white", width=8, height=2,command=lambda: (telasDeData.proxTela(),
+                                                                                                                     telaUm.destroy()))
+        continuar.pack()
+        continuar.place(x=120,y=250)
+        checkinDentro = checkin_date.get_date()
+        checkoutDentro = checkout_date.get_date()
+
+        telaUm.mainloop()
+    
 class telasDeQuartos:
 
     def segundaTela():
@@ -54,6 +100,7 @@ class telasDeQuartos:
             btnBox.pack()
             btnBox.place(x=x, y=y)
 
+
 class Cadastro:
      def cadastro(quarto):
         telaTres = tk.Tk()
@@ -67,6 +114,7 @@ class Cadastro:
             nome = nameEntry.get()
             cpf = cpfEntry.get()
             database.insert_room(nome,cpf,quarto,1)
+            database.insert_checks(checkinDentro,checkoutDentro,cpf)
 
         rotuloDoTitulo = Label(telaTres, bg="dark blue", width=180, height=6)
         rotuloDoTitulo.pack()
@@ -94,7 +142,24 @@ class Cadastro:
         salvarDados.pack()
         salvarDados.place(x=380, y=190)
         
-        occupied = database.select_occupied(quarto,)
+    
 
         def proxTela():
-            telaResumo.TelaDeResumo.quartaTela()
+            TelaDeResumo.quartaTela()
+            
+class TelaDeResumo:
+    def quartaTela():
+        telaQuatro = tk.Tk()
+        telaQuatro.geometry("300x300")
+        telaQuatro.minsize(300, 300)
+        telaQuatro.maxsize(300, 300)
+        telaQuatro.title("Resumo")
+    
+
+
+
+
+
+
+
+
